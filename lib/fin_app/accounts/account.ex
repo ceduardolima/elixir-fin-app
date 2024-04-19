50d9eb474032b637afd816de332ea8856cc2c909
Eduardo <ceduardolima2000@gmail.com>
2024-04-19 09:45:33 -0300

    Criando hash para a senha

--- lib/fin_app/accounts/account.ex
+++ lib/fin_app/accounts/account.ex
@@ -1,6 +1,7 @@
 defmodule FinApp.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
+  require Logger
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -15,9 +16,24 @@ defmodule FinApp.Accounts.Account do
 
   @doc false
   def changeset(account, attrs) do
-    account
+    value = account
     |> cast(attrs, [:email, :hash_password])
     |> validate_required([:email, :hash_password])
     |> validate_format(:email, ~r/@/)
+    |> put_password_hash()
+
+    Logger.info("Valor changeset: #{inspect(value)}")
+
+    value
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hash_password: hash_password}} = changeset) do
+    value = change(changeset, hash_password: Bcrypt.hash_pwd_salt(hash_password))
+    Logger.info("changeset hash: #{inspect(value)}")
+    value
+  end
+
+  defp put_password_hash(changeset) do
+    changeset
   end
 end
