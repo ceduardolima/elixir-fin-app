39ac53f655148e5f195bb5481ee383babea134b3
Eduardo <ceduardolima2000@gmail.com>
2024-04-25 09:52:53 -0300

    Usando o guardian.DB para armazenar o token criado

--- lib/fin_app_web/auth/guardian.ex
+++ lib/fin_app_web/auth/guardian.ex
@@ -2,6 +2,11 @@ defmodule FinAppWeb.Auth.Guardian do
   use Guardian, otp_app: :fin_app
   alias FinApp.Accounts
 
+#----------------------------------------------------
+# -> Gerando o token do usuário
+
+
+
   def subject_for_token(%{id: id}, _claims), do: {:ok, to_string(id)}
 
   def subject_for_token(_, _), do: {:error, :no_id_provided}
@@ -48,4 +53,32 @@ defmodule FinAppWeb.Auth.Guardian do
     {:ok, token, _claims} = encode_and_sign(account)
     {:ok, account, token}
   end
+
+#----------------------------------------------------
+# -> Salvando localmente o token e gerando um timeout
+
+  def after_encode_and_sign(resource, claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.after_encode_and_sign(resource, claims["typ"], claims, token) do
+      {:ok, token}
+    end
+  end
+
+  def on_verify(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
+      {:ok, claims}
+    end
+  end
+
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
+    end
+  end
+
+  def on_revoke(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
+      {:ok, claims}
+    end
+  end
+
 end
